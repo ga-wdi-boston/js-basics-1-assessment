c7fa8496cb4d1114f4fbee076f793fdd8dd319cf
Teo Paolucci <teo.paolucci@gmail.com>
2015-09-30 09:56:07 -0400

    Responded to questions in README, changed assesment.js to ask for a number.

--- assessment.js
+++ assessment.js
@@ -3,6 +3,7 @@ var ask = require('./ask.js');
 
 var answer = 'not empty';
 
-while (answer !== '' && answer !== 'SeCrEt') {
+while (answer !== 0 && answer !== 4) { //0 is thate to escape program without typing number, much like the empty string
   answer = ask("Guess my secret? ");
+  answer = +answer;   //changes answer from string to num
 }
