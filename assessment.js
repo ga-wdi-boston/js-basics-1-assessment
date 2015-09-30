2544c332b4c4f90a89b617abb7beeb3c99002ae0
kuramameng <mengjs324@gmail.com>
2015-09-30 09:44:55 -0400

    refine assessment.js

--- assessment.js
+++ assessment.js
@@ -3,6 +3,6 @@ var ask = require('./ask.js');
 
 var answer = null;
 
-while (answer <= 1 && answer >= 10) {
+while (answer < 1 || answer > 10) {
   answer = ask("Enter a number between 1 to 10");
 }
