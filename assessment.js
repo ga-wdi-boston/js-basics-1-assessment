b4a8a710207ccf44d03c1264f66edf8c867e61c4
Josh <toyjtoy@gmail.com>
2015-09-30 09:55:22 -0400

    finish assesment and README

--- assessment.js
+++ assessment.js
@@ -1,8 +1,12 @@
+// Change the code from this question so that it tests for a number betwen 1 and 10 instead of a string.
+// Save it in the file called `assessment.js`.
+// You can test the code with `node assessment.js` in your Terminal.
+
 //We'll learn about require later in the course
 var ask = require('./ask.js');
 
-var answer = 'not empty';
+var answer = 0;
 
-while (answer !== '' && answer !== 'SeCrEt') {
+while ((answer < 10) && (answer > -1)) {
   answer = ask("Guess my secret? ");
 }
