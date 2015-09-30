f4a410daa22f0ba9208170dbaea67f91c2339eb6
sdavidson140 <sdavidson140@gmail.com>
2015-09-30 09:55:24 -0400

    finished js assessment

--- assessment.js
+++ assessment.js
@@ -1,8 +1,8 @@
 //We'll learn about require later in the course
 var ask = require('./ask.js');
 
-var answer = 'not empty';
+var answer = 5;
 
-while (answer !== '' && answer !== 'SeCrEt') {
-  answer = ask("Guess my secret? ");
+while (answer < 10 && answer !== 5) {
+  answer = ask("Im thinking of a number between 1 and 10. What is it? ");
 }
