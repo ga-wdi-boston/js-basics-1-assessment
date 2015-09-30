d9570c515669459f9b6f732398c55d433b938cac
gmankd <pabloguzman26@yahoo.com>
2015-09-30 09:54:30 -0400

    final responses

--- assessment.js
+++ assessment.js
@@ -3,6 +3,6 @@ var ask = require('./ask.js');
 
 var answer = 'not empty';
 
-while (answer !== '' && answer !== 'SeCrEt') {
+while (answer  '' && answer !== 'SeCrEt') {
   answer = ask("Guess my secret? ");
 }
