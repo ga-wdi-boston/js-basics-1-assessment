117af9aaaed7c781988d06b9ea871914a23b576d
EdieShimel <edieshimel@gmail.com>
2015-10-25 13:50:13 -0400

    I did pretty well on this assessment the first time, but I have gone back over it, answered what I got wrong, and wrote a few comments for the last question with some suggestions/explainations of why my newbie thought process was out of sync with the question.

--- assessment.js
+++ assessment.js
@@ -1,16 +1,9 @@
 //We'll learn about require later in the course
-// var ask = require('./ask.js');
-
-// var answer = 8
-// console.log("Guess a number between 1 and 10");
-
-// while (answer != '8' && answer > '10') {
-//   answer = ask("Guess my secret? ");
-// }
 var ask = require('./ask.js');
 
-var answer = (answer < 10);
+ var answer = 'Where a guess would go'; //I changed this
 
-while (answer !== 8 && answer !== 8) {
-  answer = ask("Guess my secret? ");
-}
+ while (answer !== '' && answer !== '8') {
+   answer = ask("Guess a number between 1 and 10");
+ }
+
