5a313c9e6a11f6a316b1eeb71b04c0a779754ef7
Celena T <chibiynm@gmail.com>
2016-01-21 09:31:54 -0500

    Changed my answer

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -2,9 +2,9 @@
 //We'll learn about require later in the course
 const ask = require('../lib/ask.js');
 
-var answer = 0;
+var answer = 1;
 
-while (answer !== '') {
+while (answer > 0) {
   answer = ask("Guess my secret?");
   if (answer <= 10) {
    console.log("You guessed it!");
