6281fa8210787bd5121f79fcc7f6c83f0efa834d
Jeremiah <jeremiahdamiron@gmail.com>
2016-01-21 09:32:07 -0500

    Put answers on README

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -6,8 +6,13 @@ var answer = 'not empty';
 
 while (answer !== '') {
   answer = ask("Guess my secret? ");
-  if (answer === 'SeCrEt') {
+  if (answer === '7') {
    console.log("You guessed it!");
    break;
+ }
+  else if (answer !=== >=1 && <=10) {
+    console.log("Your guess needs to be between 1 and 10.");
+  else (answer !=== '7')
+    console.log("Wrong! Try again.");
   }
-}
+ }
