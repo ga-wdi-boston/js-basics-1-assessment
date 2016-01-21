9160576ba41dfa0c74d0d7069b9ff76af3397a58
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-01-21 11:25:38 -0500

    fixes loop bug

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -2,15 +2,15 @@
 //We'll learn about require later in the course
 const ask = require('../lib/ask.js');
 
-var answer = 'not empty';
+let answer = 'not empty';
+answer = answer.toString();
 
 while (answer !== '') {
   answer = ask("Guess my secret? ");
-  if (answer === 9) {
+  if (answer === '9') {
    console.log("You guessed it!");
- }
+ } else if (answer < '1' || answer > '10') {
+   console.log('Pick a number between 1 and 10');
    break;
- } else if (answer < 1 || answer > 10) {
-   console.log('Please choose a number between 1 and 10');
- }
+}
 }
