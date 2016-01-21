ab1db0693814017fc9ab6918c04157b83f0e14eb
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-01-21 12:27:34 -0500

    fixes additional problems
    it runs now!

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -3,14 +3,13 @@
 const ask = require('../lib/ask.js');
 
 let answer = 'not empty';
-answer = answer.toString();
 
 while (answer !== '') {
   answer = ask("Guess my secret? ");
   if (answer === '9') {
    console.log("You guessed it!");
- } else if (answer < '1' || answer > '10') {
-   console.log('Pick a number between 1 and 10');
    break;
+ } else if (answer < 1 || answer > 10) {
+   console.log('Pick a number between 1 and 10');
 }
 }
