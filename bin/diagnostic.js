3d687d460e4dfb8e3866f1040918e422c363d049
Dumplingpuff <jason.liang2012@gmail.com>
2016-01-22 09:27:29 -0500

    Complete diganostics for js basics

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,13 +1,19 @@
 'use strict';
+
+
 //We'll learn about require later in the course
 const ask = require('../lib/ask.js');
 
-var answer = 'not empty';
+let answer = 'not empty';
 
 while (answer !== '') {
-  answer = ask("Guess my secret? ");
-  if (answer === 'SeCrEt') {
+  answer = ask("I chose a number between 1 and 10, what is it? ");
+  if (answer === 2) {
    console.log("You guessed it!");
    break;
+  }
+
+  else if (answer < 1 && answer > 10) {
+    console.log("Sorry your answer is not between 1 and 10.")
   }
 }
