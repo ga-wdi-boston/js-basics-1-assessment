e71397a895e43c7c9696c538309b3b1a9376ab80
“Benjamski” <your_email@example.com>
2016-03-23 09:27:18 -0400

    diagnostic-submission js-basics
    I made that more complicated than it was but I
    think I'm just not used to using node to test atom files yet.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === 8) {
+   console.log("You guessed it!");
+   break;
+  }
+if (answer >= 1 && answer <= 10) {
+  console.log('Try Again ');
+} else if (answer !== 8) {
+  console.log('Your guess needs to be between 1 and 10 doofus');
+}
+}
