c85f7bca7c0d187ab0115b97af16e31c2d81d51e
rdelvallej32 <rdelvallej@gmail.com>
2016-03-23 09:26:24 -0400

    Answer to Diagnostic
    Both ReadMe and bin file are updated with the new answers and code.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '7') {
+  answer = ask("Guess a number between 1 and 10 ");
+  if (answer === '7') {
+   console.log("You guessed it! You should play the Powerball");
+   break;
+  }
+}
