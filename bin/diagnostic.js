7281150297c7070beb0381efc05ff0f6a144b875
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2016-03-23 09:27:32 -0400

    Fail to asnwer everything in life

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,16 @@
 'use strict';
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer !== 1 && <== 10) {
+   console.log("You guessed it!");
+   break;
+  }
+}
+```
+
+//Definitely got a one-way ticket to struggle city :(
