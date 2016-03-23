45d1967082191fe3e10d6345904ccb4013c61206
OllieBuilds <scarborough.oliver@gmail.com>
2016-03-23 09:26:58 -0400

    Changed && to || on line 12
    More robust logic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -9,7 +9,7 @@ let answer = 'not empty';
 
 while (answer !== undefined) {
   answer = ask("Guess my secret? ");
-  if (answer == NaN || (answer < 1 && answer > 10) ){
+  if (answer == NaN || (answer < 1 || answer > 10) ){
     console.log("I'll give you a hint: it is a number between 1 and 10.");
     break;
   }
