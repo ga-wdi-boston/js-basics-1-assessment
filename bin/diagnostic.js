a5e36662f9545417a8678a90801656279e0a6bd2
philschoof <phil.schoof@gmail.com>
2016-03-23 09:17:40 -0400

    Complete JS-Basics Diagnostic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === '5') {
+   console.log("You guessed it!");
+   break;
+ }else if(answer < 1 || answer > 10){
+   console.log("The guess needs to be between 1 and 10");
+ }
+}
