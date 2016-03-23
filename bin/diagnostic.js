2b98cf7b6d3786e7f461785093d4e4f193f95759
louarnos <louarnos@gmail.com>
2016-03-23 09:31:27 -0400

    Response to JS Basics Diagnostics
    Edited readme
    edited bin/diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+let myNum = 6;
+let answer = 3;
+
+while (answer !== myNum) {
+  answer = ask("Guess my number");
+  if (answer > 10 || answer < 1 ) {
+   console.log("Please Guess a number between 1 and 10!");
+ }
+ else if (answer === myNum) {
+   console.log("You guess correct!")
+   break;
+ }
+ else {
+   console.log("Not my number! Guess again");
+ }
+  }
