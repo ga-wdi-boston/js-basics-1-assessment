00756afcb05f8cabc48fda498aa8db4da8cf5896
erinsul <erin_sullivan50@yahoo.com>
2016-05-18 09:17:37 -0400

    Add initial code
    Copied the provided code and changed strings to ask for numbers instead.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,12 @@
 'use strict';
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my number between one and ten? ");
+  if (answer === '5') {
+   console.log("You guessed it!");
+   break;
+  }
+}
