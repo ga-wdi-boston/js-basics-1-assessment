1aac7a68389216e990c59bf2be21cc0355fc2a29
RoobScoobs <iruben229@gmail.com>
2016-05-18 09:24:50 -0400

    js-basics-diagnostic #5
    Change code to answer #5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 7;
+
+while (answer !== 7) {
+  answer = ask("Guess my secret? ");
+  if (answer === 7) {
+   console.log("You guessed it!");
+   break;
+ } else if (answer !== 1 < answer < 10) {
+   console.log("Needs to be between 1 and 10");
+ }
+}
