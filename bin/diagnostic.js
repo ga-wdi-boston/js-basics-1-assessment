388949dfdf058407aaea58de55a3f31f684d71e8
MishaHerscu <mishaherscu@gmail.com>
2016-05-18 09:32:45 -0400

    Actually saved file before committing changes.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -3,12 +3,14 @@
 //We'll learn about require later in the course
 const ask = require('../lib/ask.js');
 
-let myAnswer = 5;
+let answer = 'not empty';
 
 while (answer !== '') {
   answer = ask("Guess my secret? ");
-  if (answer === myAnswer) {
+  if (answer === 5) {
    console.log("You guessed it!");
    break;
-  }
+ }else if(answer > 10 || answer < 1){
+   console.log("You guessed it!");
+ }
 }
