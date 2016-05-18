8c7300d750a6c951057653aa11c6dddd17a95c3f
joechristman <jchristman4@gmail.com>
2016-05-18 09:30:01 -0400

    joe
    last question was confusing

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
-'use strict';
+//This question is confusing, not sure what you're getting at
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === 'SeCrEt') {
+   console.log("You guessed it!");
+   break;
+  }
+}
