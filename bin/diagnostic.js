03514ac8d5f220cfa9ad4093751958269249b679
tina513 <dlnaz.ai@hotmail.com>
2016-05-18 09:41:29 -0400

    this is q5
    i changed answer

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -6,10 +6,9 @@ let answer = true;
 
 while (answer !== undefined) {
   answer = ask("Pick a number between 1 to 10? ");
-  if (answer < 1 && answer > 10) {
+  if (answer < 1 || answer > 10) {
    console.log("Your number needs to be any number between 1 to 10!");
-   break;
- }else{
-  continue;
+   continue;
  }
+ break;
 }
