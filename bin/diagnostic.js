f293601f020c5cead3dd19489e63e63348411674
erinsul <erin_sullivan50@yahoo.com>
2016-05-18 09:24:03 -0400

    Add test to make sure number between 1 and 10
    Added a conditional statement to check that the number entered is between 1 and
    10 and return a message if not.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -5,7 +5,10 @@ let answer = 'not empty';
 
 while (answer !== '') {
   answer = ask("Guess my number between one and ten? ");
-  if (answer === '5') {
+  if (!(answer >= 1 && answer <=10)){
+    console.log("You need to pick a number between one and ten!")
+  }
+  else if (answer === '5') {
    console.log("You guessed it!");
    break;
   }
