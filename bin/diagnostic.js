d0a00eebc878fd981eef5fa304fa16cc27c6cbb1
mattb5 <mattbuffardi@gmail.com>
2016-05-18 09:32:45 -0400

    second commit changing a syntax error

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -6,7 +6,7 @@ let answer = 'not empty';
 
 while (answer !== '') {
   answer = ask("Guess a number between 1 and 10");
-  if ()(answer < 1 )||(answer > 10 ))
+  if ((answer < 1 )||(answer > 10 ))
    {
    console.log("The answer must be between 1 and 10")
    }
