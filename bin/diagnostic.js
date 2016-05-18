a6496446bd876e1cb6c5da1d1a8079555b50a115
acobrelo <acobrelo@gmail.com>
2016-05-18 09:25:39 -0400

    Question 5 Answer
    Question 5 answered and saved in diagnostic.js.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess a number between one and ten");
+  if (answer === '5') {
+   console.log("You guessed it!");
+   break;
+   else if (answer < 1 || answer > 10) {
+   ask("You need to guess a number between one and ten");
+   } else {
+    ask("Incorrect. Guess again");
+   }
+  };
+}
