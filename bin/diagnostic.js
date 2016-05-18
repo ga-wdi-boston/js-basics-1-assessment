e479d606a950f0499c3489c534a139c5c1b162c6
jacobhereford <jacobhereford@gmail.com>
2016-05-18 09:25:24 -0400

    js-basics-diagnostic
    answer for question 5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict';
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === '6') {
+   console.log("You guessed it!");
+   else = ask("Guess my secret. Hint is 1-10.")
+   break;
+  }
+}
