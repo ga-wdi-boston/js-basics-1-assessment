8afadfeba1de724e73050bcfd59209717868506a
cdelsoin <cdelsoin@gmail.com>
2016-07-13 13:29:16 -0400

    Answered question
    Tried to answer the questions as best as I could

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,14 @@
 'use strict';
+
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my number? ");
+  if (answer === 7) {
+   console.log("You guessed it!");
+   break;
+  }
+}
