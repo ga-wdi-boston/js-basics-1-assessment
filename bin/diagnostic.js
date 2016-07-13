1ce61027ffc5338e5e7f5c4bbff413fff8ec20b4
EchoXin <xin.yi@husky.neu.edu>
2016-07-13 13:30:26 -0400

    Add solution to diagnostic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 2;
+
+while (answer>=1&&(answer<=10)&&(answer !== 2)) {
+  answer = ask("Guess my secret? ");
+  if (answer === 2) {
+   console.log("You guessed it!");
+   break;
+  }
+  if (answer>10||(answer<1)){
+    console.log('The number is between 1 and 10');
+  }
+}
