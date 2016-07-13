ded2a99675709ad01a33211051561b806b974ccb
jordanallain <jallain@gmail.com>
2016-07-13 13:25:30 -0400

    Complete diagnostic assignment
    Filled out the answers for questions 1-4 and attempted the
    javascript problem from question 5.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//We'll learn about require later in the course
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== ''){
+  answer = ask("Guess my secret? ");
+    if(answer <= 1 || answer >= 10){
+      console.log('Please enter a number between 1 and 10');
+    }else if(answer === 8){
+        console.log('You guessed it!');
+        break;
+    }else{
+      console.log('Try Again!');
+    }
+  }
