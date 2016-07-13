0ab40d579c8aa20efcb0fbdfb90ba0722904dea2
EchoXin <xin.yi@husky.neu.edu>
2016-07-13 14:51:47 -0400

    Modify the solute of Q5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -2,15 +2,15 @@
 
 const ask = require('../lib/ask.js');
 
-let answer = 2;
+let answer = 'not empty';
 
-while (answer>=1&&(answer<=10)&&(answer !== 2)) {
+while (answer !== '') {
   answer = ask("Guess my secret? ");
-  if (answer === 2) {
+  if (answer === '2') {
    console.log("You guessed it!");
    break;
   }
-  if (answer>10||(answer<1)){
-    console.log('The number is between 1 and 10');
+  if (answer > '10'||(answer < '1')){
+    console.log("The number is between 1 to 10");
   }
 }
