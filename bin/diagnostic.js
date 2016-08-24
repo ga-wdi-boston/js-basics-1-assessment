795d9ba78949b1c0d2ad3b5e04fe13a7c40a911d
nishkixs <nishon.radhakrishnan@gmail.com>
2016-08-24 09:23:51 -0400

    Question 5 SEcret

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,15 @@
 'use strict';
+
+js
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = 'SeCrEt';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === 'SeCrEt') {
+   console.log("You guessed it!");
+   break;
+  }
+}
