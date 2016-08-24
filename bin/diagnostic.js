f26d7939df5195034f7fbf0c439e8dd33109b724
rebekahheacock <rebekah.heacock@gmail.com>
2016-08-24 09:11:16 -0400

    Remove extraneous comments
    Remove comments from bin/diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,13 +1,4 @@
 'use strict';
-//We'll learn about require later in the course
-
-/*Change the code from this question so that it tests for a number of your choice
-between 1 and 10 instead of the string `'SeCrEt'`.  If `answer` isn't >= 1 and
-<= 10, use `console.log` to display a message saying that the guess needs to
-be between 1 and 10.  Save your code in the file called `bin/diagnostic.js`.
-You can test the code with `node bin/diagnostic.js` in your Terminal.
-*/
-
 
 const ask = require('../lib/ask.js');
 
