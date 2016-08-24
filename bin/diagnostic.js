10da20af27ef24a09f4129dd48ba5a9cce04baa4
bullhead242 <bullhead242@gmail.com>
2016-08-24 09:34:19 -0400

    Add response to 8/24 JS basics diagnostic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === '7') {
+   console.log("You guessed it!");
+   if (answer <= 0 || answer>10) {
+     console.log("A number between 1 and 10, please. Thank you.");
+   }
+   break;
+}
+  }
+'use strict';
