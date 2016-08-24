65784688826b8818edc16e30626e0ac129d06204
chrxtina <christina.ad.nguyen@gmail.com>
2016-08-24 09:23:33 -0400

    Add answers to js-basics-diagnostic
    Answers in diagnostic.md and diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Type a number between 1 and 10.");
+  if (answer >= 1 && answer <= 10) {
+   console.log("Correct!");
+   break;
+  }
+  else {
+    console.log("Your number needs to be between 1 and 10!");
+    break;
+  }
+}
