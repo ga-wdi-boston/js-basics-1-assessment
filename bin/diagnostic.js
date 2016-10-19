ab823616e11fb65709537d870c8676117c3aace0
akadane87 <akadane87@gmail.com>
2016-10-19 13:24:49 -0400

    JS - Basics Diagnostic complete

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === 'SeCrEt') {
+   console.log("You guessed it!");
+   break;
+  }
+}
+
+
+// Change the code from this question so that it tests for a number of your choice
+// between 1 and 10 instead of the string `'SeCrEt'`.  If `answer` isn't >= 1 and
+// <= 10, use `console.log` to display a message saying that the guess needs to
+// be between 1 and 10.  Save your code in the file called `bin/diagnostic.js`.
+// You can test the code with `node bin/diagnostic.js` in your Terminal.
