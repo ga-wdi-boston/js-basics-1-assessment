6eeb569a5f4ca2653f971ae321ce706dbf8d50d1
kiraamaa <kira.a.maclean@gmail.com>
2016-10-19 13:23:49 -0400

    Begin solving problem #5 afternoon diagnostic
    Complete most of code for problem #5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,21 @@
 'use strict';
+
+## Question 5
+
+
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === '7') {
+   console.log("You guessed it!");
+   break;
+ } else if (answer >= 10) {
+   console.log("Guess a number between 1 and 10.");
+ } else if (answer <= 1) {
+   console.log("Guess a number between 1 and 10.");
+ }
+}
