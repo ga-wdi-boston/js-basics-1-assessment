66cf13bece0e99514ae7632eabddd7350ca82528
kibs86 <kibs086@gmail.com>
2016-10-19 14:00:19 -0400

    Update diagnostic.js
    Fix diagnostic.js so it accepts the correct number and tells the user
    they guessed correctly.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -2,14 +2,14 @@
 
 const ask = require('../lib/ask.js');
 
-let answer = 'not empty';
+let answer = NaN;
 
-while (answer !== '') {
-  answer = ask("Guess my number? Hint: It's between 1 and 10. ");
+while (answer !== NaN) {
+  answer = +(ask("Guess my number? Hint: It's between 1 and 10. "));
   if (answer < 1 || answer > 10) {
     console.log("Your guess needs to be between 1 and 10.");
   } else if (answer === 3) {
     console.log("You guessed it!");
-    break;
+    answer = NaN;
   }
 }
