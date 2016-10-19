4b9276829caad21b6367d5d3449e28f59d4d7d54
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-10-19 13:22:20 -0400

    Add response to question#5 for js-basic-diagnostic
    Add response to question#5 for js-basic-diagnostic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = 0;
+
+while (answer !== 5) {
+  answer = ask("Guess my number? ");
+  if (answer >= 1 && answer <= 10) {
+   console.log("Needs to be between 1-10.");
+  }
+  else {
+   console.log("You guessed it!");
+   break;
+  }
+}
