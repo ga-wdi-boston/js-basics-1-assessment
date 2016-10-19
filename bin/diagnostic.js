014fb8e58a6d59d2f9236e27041ecfe747394c60
Naki21 <Yianni21@gmail.com>
2016-10-19 15:34:38 -0400

    Fixed Diag
    Tweeked conditional

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,12 +1,11 @@
 'use strict';
-
 const ask = require('../lib/ask.js');
 
 let answer = 'not empty';
 
 while (answer !== '') {
   answer = ask("Guess my secret? ");
-  if (answer >= 1 || <= 10) {
+  if (answer >= 1 && answer <= 10) {
    console.log("You guessed it!");
    break;
  } else {
