646489fcbedd846bf47512b98467dbea8f1e5d75
mwb86 <masonwbrown@gmail.com>
2016-11-16 13:36:44 -0500

    added answers

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,29 @@
 'use strict';
+## Question 5
+
+```js
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = '7';
+
+while (answer !== '7') {
+  answer = ask("Guess my secret? ");
+  if (answer === '7') {
+   console.log("You guessed it!");
+   break;
+  }
+  else if ((answer <= 1 || answer >= 10) === true ){
+  console.log("pick a number between 1 and 10"}
+
+  else{
+    console.log("try again");
+  }
+}
+```
+
+Change the code from this question so that it tests for a number of your choice
+between 1 and 10 instead of the string `'SeCrEt'`.  If `answer` isn't >= 1 and
+<= 10, use `console.log` to display a message saying that the guess needs to
+be between 1 and 10.  Save your code in the file called `bin/diagnostic.js`.
+You can test the code with `node bin/diagnostic.js` in your Terminal.
