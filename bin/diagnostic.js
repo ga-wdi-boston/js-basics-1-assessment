8bfd4ef89d4fb3bc021bc95035a24c797b7ccb0a
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-01-11 14:29:35 -0500

    Need more time didn't have enough time for number 5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,17 @@
 'use strict';
+
+```js
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer = 0;
+
+while (answer !== (>=1 && <=10) {
+  answer = ask("choose a number between 1-10");
+  if (answer === ) {
+   console.log("Your input must be a number between 1 and 10");
+   break;
+  }
+}
+```
+NOT ENOUGH TIME TO FINISH
