0253e5fbdecc5634951a9ddb16ea52fbff6a282a
skylarkJ <janaryndin@gmail.com>
2017-01-11 14:28:30 -0500

    4 questions add

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,15 @@
 'use strict';
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+
+  for (number = 1; number < 10; number++) {
+    if (1 <= answer <= 10) {
+      console.log("You are correct.");
+    } else {
+      console.log("Try again, your guess must be between 1 and 10 included.");
+    }
+}
