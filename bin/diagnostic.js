26b96c15866add1a8a43f3ffee8a44f0e7d4d3e4
jwaskow <thewasko@gmail.com>
2017-01-11 14:31:42 -0500

    complete diagnostic
    completed
    
    Body in detail

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,16 @@
 'use strict';
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer < 1 && answer > 10) {
+    console.log('The guess needs to be between 1 and 10');
+    ask("Guess my secret? ");
+  }
+  else if (answer === '7') {
+   console.log("You guessed it!");
+   break;
+  }
+}
