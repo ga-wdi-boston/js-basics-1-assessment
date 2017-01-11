49bcb5a6eac6757a3ad819b6f7c4d1547168075c
lbreguet <louis-w@live.com>
2017-01-11 14:24:18 -0500

    Complete JS diagnostic.
    -clarity: 5
    -comfort: 4

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const ask = require('../lib/ask.js');
+
+let answer = 'n';
+
+while (answer !== '5') {
+  answer = ask("Guess my secret? ");
+  if (answer === '5') {
+   console.log("You guessed it!");
+ } else {
+   console.log('I will give you a hint. It is between 1 and 10');
+ }
+}
