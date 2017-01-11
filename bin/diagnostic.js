a0f85a05c0239b29dad156ed7b17111a3d1abccc
yourUsername <your_email@example.com>
2017-01-11 14:28:09 -0500

    answered question 5- I think!

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict';
+const ask = require('../lib/ask.js');
+
+let answer = 'not empty';
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ");
+  if (answer === 5) {
+   console.log("You guessed it!");
+ } else
+ {console.log ("The guess needs to be between 1 and 10");
+}
+}
