22aa2ead3445260788f371a7d8fa6e5f517df671
deeforte <deirdre.forte@libertymutual.com>
2017-03-23 13:37:40 -0400

    Committing js-diagnostic
    Description here

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,12 @@
 'use strict'
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Guess a number from 1 to 10.")
+  if (answer < 1 || answer > 10) {
+   console.log("Guess must be a number from 1 to 10!")
+   break
+  }
+}
