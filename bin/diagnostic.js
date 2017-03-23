81209d825f51bcb4758cd05c8d73278988213d06
jim-moody <jim.moody@libertymutual.com>
2017-03-23 13:27:57 -0400

    Add answer to question 5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask('Guess my secret? ')
+  if (answer < 1 || answer > 10) {
+    console.log('Sorry, your guess needs to be between 1 and 10!')
+    break
+  }
+}
