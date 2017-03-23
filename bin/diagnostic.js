3646c10d13324ad1591e5a6b1c1beb9020df2c1f
deeforte <deirdre.forte@libertymutual.com>
2017-03-23 13:47:40 -0400

    Commit after time called
    Kept working on it after time was called
    This is the second commit.   Pull first if you'd like

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -4,9 +4,9 @@ const ask = require('../lib/ask.js')
 let answer = 'not empty'
 
 while (answer !== '') {
-  answer = ask("Guess a number from 1 to 10.")
-  if (answer < 1 || answer > 10) {
-   console.log("Guess must be a number from 1 to 10!")
+  answer = ask("Guess a number between 1 to 10? ")
+  if (answer >= 1 && answer <= 10) {
+   console.log("You guessed it!")
    break
   }
 }
