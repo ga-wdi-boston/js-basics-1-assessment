f8e047c90c5cfe26c92cc990508117a1d54c2f10
“conorjennings” <conorjennings@gmail.com>
2017-03-23 13:54:16 -0400

    Fixed the bug in my code that was using three equals to test for the number

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -5,7 +5,7 @@ let answer
 
 while (answer !== '') {
   answer = ask("Guess my secret? ")
-  if (answer === 6 && (answer > 0 && answer <= 10)) {
+  if (answer == 6 && (answer > 0 && answer <= 10)) {
    console.log("You guessed it!")
    break
   }
