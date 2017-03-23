9ef4f3cb411be9d2a608d0878974437c4ae24547
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-03-23 13:24:19 -0400

    Entered responses for JS diagnostic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask('Guess my number? ')
+  if (answer === '7') {
+    console.log('You guessed it!')
+    break
+  }
+  if (answer < 1 || answer > 10) {
+    console.log('Please enter a number between 1 and 10')
+  }
+}
