812e9b31e5bc92105a575d9b95695baeae918903
kqngo <khan.ngo@gmail.com>
2017-03-23 13:26:21 -0400

    Add response for diagnostic.md and diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,18 @@
 'use strict'
+
+
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Guess my secret number between 1 and 10? ")
+  if (answer < 1 || answer > 10) {
+  console.log("Your guess needs to be a number between 1 and 10")
+  console.log("Guess my secret number between 1 and 10.")
+} else if (answer === 'SeCrEt') {
+   console.log("You guessed it!")
+   break
+  }
+}
