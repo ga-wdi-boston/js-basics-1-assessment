e4797a401cc65b1018ba5831dff21706488387ff
sjruby <samuel.ruby@libertymutual.com>
2017-03-23 13:43:23 -0400

    Adding answer to #5
    No changes to fist of 5 rankings, I think answer #5 is working now
    but I want to do more practice on this thing

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -5,10 +5,12 @@ const ask = require('../lib/ask.js')
 const answer = 5
 let guess = 0
 
-
 while (guess !== answer) {
   guess = parseInt(ask('Guess my secret?'))
-  if (guess > 1 && guess < 10) {
-    console.log('Number is supposed to be 1-10' + 'You guessed ' + guess)
+
+  if (guess >= 1 && guess <= 10) {
+    console.log('You guessed ' + guess + '...Guess Again')
+  } else if (guess < 1 || guess > 10) {
+    console.log('You guessed ' + guess + 'NUMBER BETWEE 1-10')
   } else { console.log('You guessed it!') }
 }
