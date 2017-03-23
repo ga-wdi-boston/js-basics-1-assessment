2621160c5b4a037c3a58dbb5e1cf9e52e6978be8
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-03-23 13:37:18 -0400

    Questions 5 but did not finish - did not test before time was up

---
+++ diagnostic.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Pick a number between 1 to 10!")
+  if (answer = 4) {
+   console.log("You guessed it!")
+   break
+ } else if {
+   (answer !== 4)
+   console.log("Nope you lose do not pass go!")
+ }
+}
