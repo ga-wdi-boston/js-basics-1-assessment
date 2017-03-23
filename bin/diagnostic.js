2a9e6cf120def446e92fdc7c1c1b2565b52eaacd
kqngo <khan.ngo@gmail.com>
2017-03-23 14:33:07 -0400

    Adding functionality to gussing game diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -7,10 +7,12 @@ let answer = 'not empty'
 while (answer !== '') {
   answer = ask('Guess my secret number between 1 and 10? ')
   if (answer < 1 || answer > 10) {
-    console.log('Your guess needs to be a number between 1 and 10')
-    console.log('Guess my secret number between 1 and 10.')
-  } else if (answer === 'SeCrEt') {
-    console.log('You guessed it!')
+    console.log('Your answer was ' + answer + '. Your guess needs to be a number between 1 and 10')
+    console.log('Guess a new number.')
+  } else if (answer === '2') {
+    console.log('You entered ' + answer + '. You guessed it!')
     break
+  } else {
+    console.log('Your answer was ' + answer + '. Its not my secret number. Try again.')
   }
 }
