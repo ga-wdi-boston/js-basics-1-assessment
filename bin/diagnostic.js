a780b5d44f3a2d979781ab930c93c45bdaa307fc
rlarsonil71 <ron.larson@libertymutual.com>
2017-03-23 13:29:41 -0400

    Added new diagnostic.js file for JS-DIAGNOSTIC

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,14 @@
 'use strict'
+
+// We'll learn about require later in the course
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask('Enter a number between 1 and 10... ')
+  if ((answer < 1) || (answer > 10)) {
+    console.log('Incorrect:  Guess needs to be between 1 and 10')
+    break
+  }
+}
