4df77e9dfac42492ccbc2133a44124277dc73c40
joehsuLM <joseph.hsu@libertymutual.com>
2017-03-23 13:20:05 -0400

    Submitting answers for js-diagnostic
    bin/diagnostic.js and diagnostic.md have been updated.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ")
+  if (answer < 1 || answer > 10) {
+   console.log("Your guess must be between 1 and 10.")
+   break
+  }
+}
