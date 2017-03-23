69c12ace4b4da4cd9f12072a07932a03d2018d79
jonifallon <joni.fallon@libertymutual.com>
2017-03-23 13:32:50 -0400

    Fallon submission
    Not complete but I tried

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,15 @@
 'use strict'
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ")
+  if (answer !>= 1 && answer !<= 10) {
+   console.log("Guess needs to be between 1 and 10")
+   break
+  }
+}
+
+//I ran out of time and know this doesn't work, but I think a ternary
+//evaluation would work
