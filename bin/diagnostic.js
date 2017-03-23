9721dcbb937533fbdb84ff7bea6cf057bf36767f
vanduong28 <vanduong28@gmail.com>
2017-03-23 13:25:05 -0400

    JS Basics Diagnostics
    Response to JS diagnostics with changes in two files.

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict'
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask('Guess my number')
+  if (answer >= 1 && answer <= 10) {
+    console.log('You guessed it!')
+  } else {
+    console.log('Guess needs to be between 1 and 10')
+  }
+}
