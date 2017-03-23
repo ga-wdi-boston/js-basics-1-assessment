84f93946e964d148703887182f8b2051884b493e
angmas <angela.mashlan@safeco.com>
2017-03-23 13:36:50 -0400

    Updated logic

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,18 @@
 'use strict'
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Guess my number? ")
+  if ((answer >= 1) && (answer <= 10)){
+    if (answer === 4) {
+         console.log("You guessed it!")
+    } else {
+      console.log("Sorry!")
+    }
+   break
+ } else {
+   console.log("The guess needs to be between 1 and 10")
+ }
+}
