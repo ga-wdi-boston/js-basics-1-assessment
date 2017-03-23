8eda4b9dc128b92ff4f4e5c8af3c95f0af3bd654
“conorjennings” <conorjennings@gmail.com>
2017-03-23 13:35:34 -0400

    Commiting the JS script for the JS Diagnostic assignment. Note: Ran out of time

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,14 @@
 'use strict'
+const ask = require('../lib/ask.js')
+
+let answer
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ")
+  if (answer === 6 && (answer > 0 && answer <= 10)) {
+   console.log("You guessed it!")
+   break
+  }
+  else
+  console.log("You need to guees between 1 and 10")
+}
