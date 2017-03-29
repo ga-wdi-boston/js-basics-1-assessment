b6852a3e39b63aba0c39ac42d2fef85c4714ebad
xiangcatherine <xiang.catherine@gmail.com>
2017-03-29 14:26:26 -0400

    Changed code from diagnostic.md

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let number = ask("Guess a number!")
+if (number >= 1 && number <= 10) {
+  console.log('Great job!')
+} else {
+  console.log('Oops! The number needs to be 1 and 10.')
+}
