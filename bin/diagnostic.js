e1c4972b98abaf5f2bff88a7754a23c327fd258f
rowlandhill <hill.rowland@gmail.com>
2017-03-29 14:38:55 -0400

    Attempted to write code: "Error: Cannot find module 'readline-sync'"

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 7
+while (answer !== 7) {
+  answer = ask("Guess my number? ")
+  if (answer <= 1 && answer >= 10) {
+   console.log("The guess needs to be between 1 and 10")
+   break
+  }
+}
+
+
+  //Change the code from this question so that it tests for a number of your choice
+  //between 1 and 10 instead of the string `'SeCrEt'`.  If `answer` isn't >= 1 and
+  //<= 10, use `console.log` to display a message saying that the guess needs to
+  //be between 1 and 10.  Save your code in the file called `bin/diagnostic.js`.
+  //You can test the code with `node bin/diagnostic.js` in your Terminal.
