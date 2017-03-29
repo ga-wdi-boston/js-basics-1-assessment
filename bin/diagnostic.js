5123de9f81aedafd6e9dbdfcd1f2970872caaf9e
pjliddy <pjliddy@gmail.com>
2017-03-29 14:22:45 -0400

    Completed js-diagnostic assignment
    Updated diagnostic.md with answers 1-4 and bin/diagnostic.js with
    the code assignment for answer 5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask('Guess my secret? ')
+
+  // If answer isn't >= 1 and <= 10, use console.log to display a message saying that the guess needs to be between 1 and 10.
+  if (answer < 1 || answer > 10) {
+    console.log('Your guess needs to be between 1 and 10.')
+
+  // Change the code from this question so that it tests for a number of your choice between 1 and 10 instead of the string 'SeCrEt'.
+  } else if (answer === '7') {
+    console.log('You guessed it!')
+    break
+  }
+}
