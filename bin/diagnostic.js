715de2c6198cdbce74d351bea7a04504913f5c1b
arlofeirman <arlofeirman@gmail.com>
2017-03-29 14:33:10 -0400

    Completed questions in diagnostic.md partially  with diagnostic.js

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 5
+
+while (answer !== '') {
+  answer = ask("Guess a number between 1 and 10 ")
+  if (answer === 5) {
+   console.log("You guessed it!")
+   break
+ } else if (answer >= 10 || <= 1){
+ console.log('The number must be between 1 and 10')
+ }
+}
