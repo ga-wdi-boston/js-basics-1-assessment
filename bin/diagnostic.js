d182d4887a31caaef6ef9c1f3b7d851111eb0341
bcem2004 <ehayden@gmail.com>
2017-03-29 14:32:49 -0400

    Change code for answer 5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,14 @@
 'use strict'
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask("Guess my secret? ")
+  if (answer === < 1 && > 10) {
+   console.log("You guessed it!")
+   break
+ } else {
+   console.log ("Please enter a number between 1-10.")
+ }
+}
