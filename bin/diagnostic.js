d36602259c79270d42ccc77951f125f600cb5d06
Maggicorp <mvjcasey@gmail.com>
2017-03-29 14:21:21 -0400

    Add diagnostic.js and diagnostic.md
    add two files, completed

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const ask = require('../lib/ask.js')
+
+let answer = 'not empty'
+
+while (answer !== '') {
+  answer = ask('Guess the number, it is between 1 - 10 inclusive: ')
+  if (answer<1 || answer>10){
+    console.log("Invalid, Please guess a number between 1 and 10.")
+  } else if (answer === '3') {
+   console.log("You guessed it! The number is 3")
+   break
+  }
+}
