a751d93a3f8b10ef22c4d2b27eedf44cf6f212f2
Pouk91 <pinaro.ouk@gmail.com>
2017-03-29 14:29:10 -0400

    JS-Diagnostic Question 5 response

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,13 @@
 'use strict'
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js')
+
+let answer = '>= 1 || <= 10'
+
+while (answer !== '3') {
+  answer = ask("Guess my secret? ")
+  if (answer === '5') {
+   console.log("You guessed it!")
+   break
+  }
+}
